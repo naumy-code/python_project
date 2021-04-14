9024ce526b4c8594e8bea84267990062b8912dd1
naumy <3111544162@qq.com.com>
2021-04-14 16:00:05 +0800

    提交QMD的测试用例代码

--- excel_test/excel_test.py
+++ excel_test/excel_test.py
@@ -3,7 +3,11 @@ import pandas as pd
 import os
 
 '''
-    columns函数返回的是列数 print(data.columns)
+    实现功能: 给定某一行的几个数值，得到这一行的全部信息
+    columns函数返回的是列数: print(data.columns)
+    获取行数: len(data)
+    获取某一列的数据: data['姓名']
+    获取前十行符合条件的数据: data[(data.姓名 == '李四') & (data.成绩 == 12) & (data.分数 == 3)][:10] 
 '''
 
 def excel_test(param1, param2, param3):
@@ -16,23 +20,20 @@ def excel_test(param1, param2, param3):
     if os.path.exists(excel_path):
         print("文件存在")
         data = pd.read_excel(excel_path, sheet_name='param')
-        # print(data)
-        for i in data.rows:
-            grade = data['成绩']
-            print(grade)
-
-
-
-
+        # print(data.loc['李四'])
+        print(data.loc[data['姓名'] == param1, ['姓名', '分数']])
+        print(data[(data.姓名 == '李四') & (data.成绩 == 12) & (data.分数 == 3)][:10])
+        data1 = data[(data.姓名 == '李四') & (data.成绩 == 12) & (data.分数 == 3)]
+        print("result", data1['分数'])
 
 
     return result
 
 
 if __name__ == '__main__':
-    param1 = 20
-    param2 = 28
-    param3 = 3
+    param1 = '李四'
+    param2 = 12
+    param3 = 4
     excel_test(param1, param2, param3)
 
 
