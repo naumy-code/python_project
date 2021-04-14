48fcbc7dd771903ae7d3a9d1a4bcdce48eeff4e4
naumy <3111544162@qq.com.com>
2021-04-14 12:23:37 +0800

    提交QMD的测试用例代码

--- excel_test/excel_test.py
+++ excel_test/excel_test.py
@@ -1,3 +1,16 @@
 import numpy as np
 import pandas as pd
+import os
+
+# 绝对路径
+excel_path1 = 'C:\\Users\\ywwei\\Desktop\\test.xlsx'
+# 相对路径
+excel_path = '../spec/test.xlsx'
+
+# 判断文件是否存在
+if os.path.exists(excel_path):
+    print("文件存在")
+    data = pd.read_excel(excel_path)
+    print(data)
+
 
