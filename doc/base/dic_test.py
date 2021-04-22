dd1348b7f87eb66cceb0688c0ecc3872f9b0db89
naumy <3111544162@qq.com.com>
2021-04-22 16:31:54 +0800

    提交QMD的测试用例代码

---
+++ doc/base/dic_test.py
@@ -0,0 +1,34 @@
+"""
+    实现需求： 动态的使用Excel获取表头作为字典的key值，value值为随机生成。
+    例如：
+        config = {
+            ”表头一“： ”随机值一“
+            ”表头二“： ”随机值二“
+            ”表头三“： ”随机值三“
+        }
+"""
+
+class auto_excel_dict:
+    def auto_excel(self):
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    list_enum = ['1', '2', '3', '4', '5']
+
+    dic = {
+        "1": "v1",
+        '2': "v2",
+    }
+    for a in list_enum:
+        print(a)
+        dic[a] = 'bbb'
+
+    print(dic)
+
+
--- doc/pandas_test/ergodic_loop.py
+++ doc/pandas_test/ergodic_loop.py
@@ -12,14 +12,14 @@ class ErgodicLoop:
         df = pd.read_excel(excel_path, sheet_name='param')
         print(df)
         # 输出每一行
-        for row in df.iterrows():
-            print(row[0], row[1])
+        # for row in df.iterrows():
+        #     print(row[0], row[1])
         # 输出每一行
         for row in df.itertuples():
             print(getattr(row, '姓名'), getattr(row, '成绩'))
         # 按列遍历iteritems():
-        for index, row in df.iteritems():
-            print(index)  # 输出列名
+        # for index, row in df.iteritems():
+        #     print(index)  # 输出列名
 
 
 if __name__ == '__main__':
